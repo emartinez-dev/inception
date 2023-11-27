458de9a63b7450403cc3e2e2d5371d1c13a58d5a
emartinez <enri.fc.98@gmail.com>
2023-11-27 17:38:04 +0100

    feat(wordpress): finally working, still not reading css and js, it was a problem with hostnames

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -21,7 +21,7 @@ else
 	define( 'DB_NAME', '$DB_NAME' );
 	define( 'DB_USER', '$DB_USER' );
 	define( 'DB_PASSWORD', '$DB_PASSWORD' );
-	define( 'DB_HOST', 'franmart.42.fr' );
+	define( 'DB_HOST', 'mariadb' );
 	define( 'DB_CHARSET', 'utf8' );
 	define( 'DB_COLLATE', '' );
 	define( 'FS_METHOD', 'direct');
