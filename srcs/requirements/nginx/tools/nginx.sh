093d41f6647efcd10431cec4b53fa1a5826505f3
emartinez <enri.fc.98@gmail.com>
2023-11-27 18:29:15 +0100

    refactor: clear the nginx container

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
 
+# Reference:
+# https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/ssl/private/nginx-selfsigned.key \
 	-out /etc/ssl/certs/nginx-selfsigned.crt \
